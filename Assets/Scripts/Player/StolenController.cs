31db9e1812f3cdcaf0f4053745afc797bb2d21cb
franlevin <54724159+franlevin@users.noreply.github.com>
2023-04-02 00:35:34 -0300

    Primordial dash created

--- Assets/Scripts/Player/StolenController.cs
+++ Assets/Scripts/Player/StolenController.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class StolenController : MonoBehaviour
 {
-
+    //Gravity
     public float gravity;
     public Vector2 velocity;
+
+    //Jumping (Need to clean)
     public float jumpVelocity = 20;
     public float groundHeight = 0;
-    //public bool isGrounded = false;
     private float minDistanceToGroundForJump = 1f;
-
     public bool isHoldingJump = false;
     public float maxHoldJumpTime = 0.4f;
     public float holdJumpTimer = 0.0f;
-
     public float jumpGroundThreshold = 1;
-    public float playerSpeed = 4f;
 
+    //Speed
+    private float playerSpeed = 50f;
+    public float normalSpeed = 50f;
+    public float dashSpeed = 100f;
+
+    //Grounded control
     public float playerHeight;
     Vector2 groundControlPos;
 
+    //For dash
+    private float dashTimer = 0.0f;
+    private float dashMaxDuration = 0.4f;
+    private bool isDashing = false;
+
+    //Input
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.D; 
 
     void Start()
     {
@@ -39,7 +53,7 @@ public class StolenController : MonoBehaviour
         if (IsGrounded() || groundDistance <= jumpGroundThreshold)
         {
             //Debug.Log("Está grounded y ground distance menor a threshold");
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(jumpKey))
             {
                 //isGrounded = false;
                 Debug.Log("Space pressed");
@@ -53,6 +67,15 @@ public class StolenController : MonoBehaviour
         {
             isHoldingJump = false;
         }
+
+        //Dash
+        if (Input.GetKeyDown(dashKey)) 
+        {
+            isDashing = true;
+            dashTimer = 0;
+            playerSpeed = dashSpeed;
+        }
+
         //transform.position.y - (-1 * (transform.localScale.y / 2))
         groundControlPos = new Vector2(transform.position.x, transform.position.y - 4f);
         Vector2 rayDirection = Vector2.down;
@@ -64,12 +87,19 @@ public class StolenController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        JumpControl();
+        DashControl();
+
+    }
+
+    private void JumpControl()
+    {
         Vector2 pos = transform.position;
         pos += Vector2.right * Time.deltaTime * playerSpeed;
 
         if (isHoldingJump || !IsGrounded())        //(!IsGrounded())
         {
-           
+
             holdJumpTimer += Time.fixedDeltaTime;
             if (holdJumpTimer >= maxHoldJumpTime)
             {
@@ -89,8 +119,21 @@ public class StolenController : MonoBehaviour
             }*/
         }
 
-
         transform.position = pos;
+    }
+
+    private void DashControl()
+    {
+        if (isDashing && dashTimer < dashMaxDuration) { dashTimer += Time.fixedDeltaTime; }
+        else { isDashing = false; dashTimer = 0; playerSpeed = normalSpeed; }
+    } 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Eye")
+        {
+            
+        }
     }
 
     private bool IsGrounded()
