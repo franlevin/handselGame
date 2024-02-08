b90c86e3d725b96a0ee97e718ce2550774b85a19
Brian Nanzer <brian.nanzer@gmail.com>
2024-02-08 00:33:25 -0300

    Pickup, nuevos módulos

--- Assets/Scripts/UI/UIScore.cs
+++ Assets/Scripts/UI/UIScore.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class UISCore : MonoBehaviour
+public class UIScore : MonoBehaviour
 {
     private ScoreKeeper scoreKeeper;
     private TextMeshProUGUI scoreText;
+    private TextMeshProUGUI scoreUpdateNotification;
+    [SerializeField] private float scoreUpdateDisplayTime = 1;
 
     private void Awake() {
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
-        scoreText = GetComponentInChildren<TextMeshProUGUI>();
+        scoreText = GetComponentsInChildren<TextMeshProUGUI>()[0];
+        scoreUpdateNotification = GetComponentsInChildren<TextMeshProUGUI>()[1];
     }
 
      private void Update() {
@@ -19,6 +22,16 @@ public class UISCore : MonoBehaviour
 
     public void UpdateScoreText(){
         int score = Mathf.RoundToInt(scoreKeeper.GetScore());
-        scoreText.text = "Score: " + score.ToString("000000000");
+        scoreText.text = score.ToString("000000000");
+    }
+
+    public void NotifyScoreIncrease(int points){
+        StartCoroutine(ScoreIncreaseNotificationDisplayDelay(points));
+    }
+
+    private IEnumerator ScoreIncreaseNotificationDisplayDelay(int points){
+        scoreUpdateNotification.text = "+" + points;
+        yield return new WaitForSeconds(scoreUpdateDisplayTime);
+        scoreUpdateNotification.text = "";
     }
 }
