7740045a26f3dba01e708d5118a0f30f9c08c21a
franlevin <54724159+franlevin@users.noreply.github.com>
2024-02-13 23:06:31 -0300

    PauseMenu y DeathMenu implementados, algunos bugs en navegación de DeathMenu

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class HUDManager : MonoBehaviour
@@ -8,11 +11,20 @@ public class HUDManager : MonoBehaviour
     public UIHealth healthUI;
     public UIScore scoreUI;
 
+    public bool gamePaused = false;
+
+    public GameObject PauseMenu;
+    public GameObject DeathMenu;
+    private GameManager gameManager;
+    public EventSystem eventSystem;
+    public GameObject retryOption;
+
+
     void Awake()
     {
        healthUI = FindObjectOfType<UIHealth>();
        scoreUI = FindObjectOfType<UIScore>();
-
+        
     }   
 
     // Start is called before the first frame update
@@ -24,18 +36,55 @@ public class HUDManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (gamePaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+
+        }
     }
 
-    void ActivateDeathHUD(){
-        if (healthUI != null)
-        {
+    private void ActivateDeathHUD(){
+        if (healthUI != null){
             healthUI.gameObject.SetActive(false);
         }
 
-        if (scoreUI != null)
-        {
+        if (scoreUI != null){
             scoreUI.gameObject.SetActive(false);
         }
+
+        DeathMenu.SetActive(true);
+        eventSystem.SetSelectedGameObject(retryOption);
     }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        DeathMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        gamePaused = false;
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void QuitToMenu()
+    {
+        ResumeGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ResetGame()
+    {
+        gameManager.ReloadScene();
+    }
+
+
 }
